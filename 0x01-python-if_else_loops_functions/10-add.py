c40acea418a8911cd755046fc9e4a075e734af7f
Uriel-229 <urielgodonou7@gmail.com>
2023-01-04 06:40:41 -0800

    mandatory

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 10-add.py
+# Uriel Godonou <Urielgodonou7@gmail.com>
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Uriel Godonou <urielgodonou7@gmail.com>
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
