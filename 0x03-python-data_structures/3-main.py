7ea09717d4d0d453ac238fddb2d055d187b3c68b
Uriel-229 <urielgodonou7@gmail.com>
2023-01-06 02:59:18 -0800

    mandatory

---
+++ 0x03-python-data_structures/3-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print_reversed_list_integer = __import__('3-print_reserved_list_integer').print_reserved_list_interger
+
+my_list = [1, 2, 3]
+print_reserved_list_integer(my_list)
